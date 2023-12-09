868585506d2d9021b9712c5037d8977133a15f06
victor <vise.c2@gmail.com>
2023-12-09 20:14:54 -0300

    Initial commit

---
+++ app/src/main/java/com/victor/victor_app/FragmentoInicio.java
@@ -0,0 +1,10 @@
+package com.victor.victor_app;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentoInicio extends Fragment {
+    // TODO: En esta clase se implementa Fragmento Inicio.
+
+
+
+}
